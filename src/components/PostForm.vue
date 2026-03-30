128bd139bf1ab368f054eaa9a1dd7caad0087745
Alana <biragova.lyudmila@gmail.com>
2026-03-30 12:38:07 +0300

    Компонент добавления поста

---
+++ src/components/PostForm.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="box">
+      <input type="text" 
+       :value="modelValue"
+        @input="emit('update:modelValue', $event.target.value)"
+      placeholder="Что у вас нового?">
+      <button @click="emit('add')">Добавить</button>
+    </div>
+</template>
+
+<script setup>
+defineProps(['modelValue'])
+const emit = defineEmits(['update:modelValue','add'])
+</script>
