08243fb2a175c550110a987d7b03543c121c2730
alinabzykova <bzykovaaa@gmail.com>
2026-04-22 17:15:02 +0300

    Создала страницу профиля

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 import FeedView from '../views/FeedView.vue'
 import StatsView from '../views/StatsView.vue'
 
-const routes =[
+const routes = [
   {
-    path:'/',
-    name:'feed',
-    component:FeedView
+    path: '/',
+    name: 'feed',
+    component: FeedView
   },
   {
-    path:'/stats',
-    name:'stats',
-    component:StatsView
-  }
+    path: '/stats',
+    name: 'stats',
+    component: StatsView
+  },
 ]
 
 const router = createRouter({
