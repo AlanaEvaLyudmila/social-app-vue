538c10a62814df99d6c4139ad61df29b9ee65cf0
AmletixQ <nazarovamridin308@gmail.com>
2026-04-23 21:56:13 +0300

    feat: сделал view страницы профиля

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,25 @@
+<template>
+  <article>
+    <h1>Страница профиля</h1>
+    <h2>{{ user.username }}</h2>
+    <p>{{ user.description }}</p>
+    <p>Count of posts: {{ user.posts.length }}</p>
+
+    <div v-if="user.posts.length > 0">
+      <PostItem v-for="post in posts" :key="post.id" :post="post" />
+    </div>
+  </article>
+</template>
+
+<script setup>
+import PostItem from '@/components/PostItem.vue';
+import { reactive } from 'vue';
+
+const user = reactive({
+  username: 'AmletixQ',
+  description: 'AmletixQ is the best',
+  posts: [],
+});
+</script>
+
+<style></style>
--- src/views/StatsView.vue
+++ src/views/StatsView.vue
@@ -1,42 +1,40 @@
 <template>
-    <div class="stats">
-        <h1>Статистика</h1>
-        <p>Всего постов: {{totalPosts}}</p>
-        <p>Всего лайков:{{totalLikes}} </p>
-        <p>Среднее количество лайков:{{averageLikes}} </p>
+  <div class="stats">
+    <h1>Статистика</h1>
+    <p>Всего постов: {{ totalPosts }}</p>
+    <p>Всего лайков:{{ totalLikes }}</p>
+    <p>Среднее количество лайков:{{ averageLikes }}</p>
 
     <div v-if="mostPopularPost">
-        <h2>Самый популярный пост</h2>
-        <p>{{mostPopularPost.text}}</p>
-        <p>Лайков: {{mostPopularPost.likes}}</p>
+      <h2>Самый популярный пост</h2>
+      <p>{{ mostPopularPost.text }}</p>
+      <p>Лайков: {{ mostPopularPost.likes }}</p>
     </div>
-
-    </div>
+  </div>
 </template>
 <script setup>
-import {ref,computed,onMounted} from 'vue'
+import { ref, computed, onMounted } from 'vue';
 
-const posts = ref([])
+const posts = ref([]);
 
-onMounted(() =>{
-    const savedPosts = localStorage.getItem('posts')
-    posts.value = savedPosts ? JSON.parse(savedPosts): []
-})
+onMounted(() => {
+  const savedPosts = localStorage.getItem('posts');
+  posts.value = savedPosts ? JSON.parse(savedPosts) : [];
+});
 
-const totalPosts = computed(()=> posts.value.length)
+const totalPosts = computed(() => posts.value.length);
 
-const totalLikes = computed(()=>{
-    return posts.value.reduce((sum,post) => sum + post.likes,0)
-})
+const totalLikes = computed(() => {
+  return posts.value.reduce((sum, post) => sum + post.likes, 0);
+});
 
-const averageLikes = computed(()=>{
-    if(posts.value.length===0)  return 0
-    return (totalLikes.value/posts.value.length).toFixed(0)
-})
+const averageLikes = computed(() => {
+  if (posts.value.length === 0) return 0;
+  return (totalLikes.value / posts.value.length).toFixed(0);
+});
 
-const mostPopularPost = computed(()=>{
-     if(posts.value.length===0)  return null
-    return [...posts.value].sort((a,b) => b.likes-a.likes)[0]
-})
-
+const mostPopularPost = computed(() => {
+  if (posts.value.length === 0) return null;
+  return [...posts.value].sort((a, b) => b.likes - a.likes)[0];
+});
 </script>
