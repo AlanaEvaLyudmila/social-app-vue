1c23215e2fbd27eb937c17f31282828db5352d50
AmletixQ <nazarovamridin308@gmail.com>
2026-04-23 21:58:06 +0300

    feat: добавил новый маршрут в файле router.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import FeedView from '../views/FeedView.vue'
-import StatsView from '../views/StatsView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import FeedView from '../views/FeedView.vue';
+import StatsView from '../views/StatsView.vue';
+import ProfileView from '@/views/ProfileView.vue';
 
-const routes =[
+const routes = [
   {
-    path:'/',
-    name:'feed',
-    component:FeedView
+    path: '/',
+    name: 'feed',
+    component: FeedView,
   },
   {
-    path:'/stats',
-    name:'stats',
-    component:StatsView
-  }
-]
+    path: '/stats',
+    name: 'stats',
+    component: StatsView,
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: ProfileView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
