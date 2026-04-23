fc04ddaf1f062897b537ee742a97a1340a59d6fb
AmletixQ <nazarovamridin308@gmail.com>
2026-04-23 21:58:49 +0300

    feat: работает навигация страниц

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="app">
+    <!-- Навигация работает -->
     <nav class="nav" style="font-size: larger; display: flex; gap: 8px;">
       <RouterLink to="/">Лента</RouterLink>
       <RouterLink to="/stats">Статистика</RouterLink>
