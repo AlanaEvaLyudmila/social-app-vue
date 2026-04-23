61522455b0e1c22ff06cf85d4b9934b22b77ac85
AmletixQ <nazarovamridin308@gmail.com>
2026-04-23 21:23:04 +0300

    feat: добавил возможность редактирования

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,18 +1,41 @@
 <template>
-    <div class="post">
-      <p>{{post.text}}</p>
+  <div class="post">
+    <div style="">
+      <p style="margin: 0" v-if="!isEditing">{{ post.text }}</p>
+      <input v-if="isEditing" placeholder="New text..." v-model.trim="edited" />
       <div class="actions">
-        <button @click="$emit('like',post.id)">
-          {{post.liked ? "❤" : "💥"}}{{post.likes}}
+        <button @click="$emit('like', post.id)">
+          {{ post.liked ? '❤' : '💥' }}{{ post.likes }}
         </button>
-      <button @click="$emit('delete',post.id)">
-        Удалить
-      </button>
+        <button @click="$emit('delete', post.id)">Удалить</button>
       </div>
     </div>
+    <div class="buttons">
+      <button v-if="!isEditing" @click="toggleEditing">Редактировать</button>
+      <button v-if="isEditing" @click="saveEditing">Сохранить</button>
+      <button v-if="isEditing" @click="cancelEditing">Отмена</button>
+    </div>
+  </div>
 </template>
 
 <script setup>
-defineProps(['post'])
-defineEmits(['like','delete'])
+import { ref } from 'vue';
+
+let isEditing = ref(false);
+let edited = ref('');
+
+function toggleEditing() {
+  isEditing.value = !isEditing.value;
+}
+function cancelEditing() {
+  isEditing.value = false;
+}
+function saveEditing() {
+  post.text = edited;
+  edited = '';
+  isEditing.value = false;
+}
+
+const { post } = defineProps(['post']);
+defineEmits(['like', 'delete']);
 </script>
--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -1,81 +1,80 @@
 <template>
   <div class="feed">
     <h1>Социальная сеть</h1>
-    <PostForm v-model="newPost" @add="addPost"/>
-    <div v-if="posts.length===0">
-      Пока нет постов
-    </div>
+    <PostForm v-model="newPost" @add="addPost" />
+    <div v-if="posts.length === 0">Пока нет постов</div>
 
     <PostItem
-    v-for="post in posts"
-    :key="post.id"
-    :post="post"
-    @like="toggleLike"
-    @delete="deletePost"
+      v-for="post in posts"
+      :key="post.id"
+      :post="post"
+      @like="toggleLike"
+      @delete="deletePost"
     />
   </div>
 </template>
 <script setup>
-  import {ref, watch,onMounted} from 'vue'
-  import PostForm from '../components/PostForm.vue'
-  import PostItem from '../components/PostItem.vue'
+import { ref, watch, onMounted } from 'vue'
+import PostForm from '../components/PostForm.vue'
+import PostItem from '../components/PostItem.vue'
 
-  const newPost = ref('')
+const newPost = ref('')
 
-  const posts = ref([])
+const posts = ref([])
 
-  onMounted(()=>{
-    const savedPosts = localStorage.getItem('posts');
-    if(savedPosts){
-      posts.value = JSON.parse(savedPosts)
-    }
-    else{
-      posts.value = [
-        {id:1, text:"Мой первый пост",likes:0}
-      ]
-    }
+onMounted(() => {
+  const savedPosts = localStorage.getItem('posts')
+  if (savedPosts) {
+    posts.value = JSON.parse(savedPosts)
+  } else {
+    posts.value = [{ id: 1, text: 'Мой первый пост', likes: 0 }]
+  }
+})
+
+watch(
+  posts,
+  (newPost) => {
+    localStorage.setItem('posts', JSON.stringify(newPost))
+  },
+  { deep: true },
+)
+
+function addPost() {
+  posts.value.unshift({
+    id: Date.now(),
+    text: newPost.value,
+    likes: 0,
   })
 
-  watch(
-    posts, (newPost) =>{
-      localStorage.setItem('posts',JSON.stringify(newPost))
-    },
-    {deep:true}
-  )
+  newPost.value = ''
+}
 
-
-  function addPost(){
-    posts.value.unshift({
-      id: Date.now(),
-      text:newPost.value,
-      likes:0
-    })
-
-    newPost.value = ''
-  }
-
-  function toggleLike(postId){
-    const post = posts.value.find(post => post.id === postId);
-    if (post) {
-      if (post.liked) {
-        post.likes--;
-        post.liked = false;
-      } else {
-        post.likes++;
-        post.liked = true
-      }
+function toggleLike(postId) {
+  const post = posts.value.find((post) => post.id === postId)
+  if (post) {
+    if (post.liked) {
+      post.likes--
+      post.liked = false
+    } else {
+      post.likes++
+      post.liked = true
     }
   }
+}
 
-  function deletePost(postId){
-    posts.value = posts.value.filter(post => post.id !== postId)
-  }
-
-
+function deletePost(postId) {
+  posts.value = posts.value.filter((post) => post.id !== postId)
+}
 </script>
 
 <style scoped>
-.post{
+.post {
+  margin-top: 10px;
+  width: 40%;
+
+  display: flex;
+  justify-content: space-between;
+
   border: 1px solid black;
   padding: 14px;
   border-radius: 10px;
