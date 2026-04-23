848d9bcbe736c7913b51f00a57128c0f252d7c1d
TsagArinaa <tsagaraevaarina@gmail.com>
2026-04-24 01:21:43 +0300

    добавила лайки и редактирование, комменты в процессе

---
+++ src/components/PostComment.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="comment">
+    <p>{{ comment.text }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps(['comment']);
+</script>
+
+<style>
+.comment {
+  border: 1px solid gray;
+  padding: 0.4rem;
+}
+</style>
--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -56,18 +56,32 @@
 
   function toggleLike(postId){
     const post = posts.value.find(post => post.id === postId);
-    if(post) post.likes++;
+    if (post) {
+      if (post.liked) {
+        post.likes--;
+        post.liked = false;
+      } else {
+        post.likes++;
+        post.liked = true
+      }
+    }
   }
 
-  function deletePost(postId){
-    posts.value = posts.value.filter(post => post.id !== postId)
-  }
+  function deletePost(postId) {
+  posts.value = posts.value.filter((post) => post.id !== postId)
+}
 
 
 </script>
 
 <style scoped>
 .post{
+  margin-top: 10px;
+  width: 40%;
+
+  display: flex;
+  justify-content: space-between;
+
   border: 1px solid black;
   padding: 14px;
   border-radius: 10px;
