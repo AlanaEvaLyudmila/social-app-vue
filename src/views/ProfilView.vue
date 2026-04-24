154068bfb72b14700c126866038fe755051ade46
TsagArinaa <tsagaraevaarina@gmail.com>
2026-04-24 23:36:45 +0300

    добавила страницу профиля

---
+++ src/views/ProfilView.vue
@@ -0,0 +1,25 @@
+<template>
+  <article>
+    <h1>Страница профиля</h1>
+    <h2>{{ user.name }}</h2>
+    <p>{{ user.description }}</p>
+    <p>Количество постов: {{ user.posts.length }}</p>
+
+    <div v-if="user.posts.length > 0">
+      <PostItem v-for="post in posts" :key="post.id" :post="post" />
+    </div>
+  </article>
+</template>
+
+<script setup>
+import PostItem from '../components/PostItem.vue';
+import { reactive } from 'vue';
+
+const user = reactive({
+  name: 'Цагараева Арина',
+  description: 'Студентка 2 курса матфака СОГУ',
+  posts: [],
+});
+</script>
+
+<style></style>
